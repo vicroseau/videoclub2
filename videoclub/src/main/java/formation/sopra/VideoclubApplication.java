9aa91cb692fce0b8cd385c3d9b0d46619c00ebdb
vicroseau <47033937+vicroseau@users.noreply.github.com>
2019-02-21 11:05:59 +0100

    Add files via upload

---
+++ videoclub/src/main/java/formation/sopra/VideoclubApplication.java
@@ -0,0 +1,13 @@
+package formation.sopra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VideoclubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VideoclubApplication.class, args);
+	}
+
+}
---
+++ videoclub/src/test/java/formation/sopra/VideoclubApplicationTests.java
@@ -0,0 +1,16 @@
+package formation.sopra;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class VideoclubApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
